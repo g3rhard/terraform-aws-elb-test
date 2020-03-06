0796d2915a728489a2d0d84fbdec0dd74d70ec64
Kiriru Trofimov <kiriru.trofimov@gmail.com>
2020-03-06 20:37:25 +0200

    add some user data for resource "example"

--- main.tf
+++ main.tf
@@ -7,6 +7,12 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t3.micro"
+
+    user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
   tags = {
     Name = "terraform-example"
   }
